fc73dad333684d2d3915cc70b3d8be6d07aea3f7
Nikolaj Dyring Jensen <nikodyring@gmail.com>
2024-06-05 02:39:19 +0200

    add timezone

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,8 @@ module SifHolbaek
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.time_zone = 'Europe/Copenhagen'
   end
 end
